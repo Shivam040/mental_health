bd6d0cb47c0b2ea12e592d564abe72cee3d00751
Shivam Dangwal <shivamdangwal04@gmail.com>
2024-10-15 10:01:22 +0530

    Revert "landing page"
    This reverts commit 7b5805eae1b9189b03870d0286aa92086a66d202.

--- client/src/pages/Lessons.jsx
+++ client/src/pages/Lessons.jsx
@@ -1,15 +1,11 @@
-import { Navigate } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import { QUERY_ME } from "../utils/queries";
-import Auth from "../utils/auth";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
+import { useState } from 'react';
 
-import { LessonContainer } from "../components";
-import { lessonData } from "../data";
-import stressbg from "../assets/stressbg123.jpg";
-import anxietybg from "../assets/depression.png";
-import depressionbg from "../assets/anxiety.png";
+import { LessonContainer } from '../components';
+import { lessonData } from '../data';
 
 const Lessons = () => {
   // If the user is not logged in, redirect to the login page
@@ -26,50 +22,54 @@ const Lessons = () => {
       question: "What is Tailwind CSS?",
       answer:
         "Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces.",
-      color: "#02C39A",
     },
     {
       question: "How does Tailwind work?",
       answer:
         "Tailwind works by providing a large set of utility classes that you can apply directly to HTML elements to style them.",
-      color: "#f6e8ae",
     },
     {
       question: "Can I customize Tailwind?",
       answer:
         "Yes, Tailwind is fully customizable. You can customize the design system by modifying the default configuration.",
-      color: "#f6e8ae",
     },
   ];
   const toggleOpen = (index) => {
     setOpenIndex(openIndex === index ? null : index);
   };
+
   return (
-    <section id="lessons" className="w-full min-h-screen p-4 md:p-8">
+    <section
+      id="lessons"
+      className="w-full min-h-screen p-4 md:p-8"
+    >
       <h1 className="h1-style mb-8 ">Check Up</h1>
       {/* Banner */}
-      <div className="mb-8 banner-container-style text-white text-shadow bg-gradient-to-r from-[#028090] to-[#00A896]">
+      <div className="mb-8 banner-container-style text-white text-shadow bg-gradient-to-r from-blue-600 to-blue-800">
         <div className="relative p-8 z-10">
-          <h2 className="banner-heading  mb-3">Welcome {user.username}!</h2>
-          <p className="text-lg">How are you feeling today ?</p>
+          <h2 className="banner-heading mb-3">Welcome {user.username}!</h2>
+          <p className="text-lg">Your adventure begins here</p>
         </div>
-        <div className="banner-bg-style " />
+        <div className="banner-bg-style bg-parkay-floor" />
       </div>
-      <LessonContainer></LessonContainer>
+      <div className="mb-8">
+        <br />
+      </div>
+      
       {/* Lessons */}
-      {/* <div className="flex flex-col gap-4 ">
+      <div className="flex flex-col gap-4 mx-10">
         {lessonData.map((lesson) => (
-          <LessonContainer key={lesson.lessonUrl} lesson={lesson} />
+          <LessonContainer
+            key={lesson.lessonUrl}
+            lesson={lesson}
+          />
         ))}
-      </div> */}
-
+      </div>
       <div className="w-full   mt-10 justify-center">
         {faqData.map((faq, index) => (
           <div
             key={index}
-            className={`${
-              faqData.color == "#02C39A" ? "bg-[#02C39A]" : ""
-            } w-[630px] md:w-full py-2 px-4 h-20  shadow-md cursor-pointer transition-transform duration-300 ease-in-out transform ${
+            className={`bg-orange-200 w-[630px] md:w-full py-2 px-2 rounded-lg shadow-md cursor-pointer transition-transform duration-300 ease-in-out transform ${
               openIndex === index ? "" : "hover:-translate-y-1"
             }`}
             onClick={() => toggleOpen(index)}
@@ -85,36 +85,7 @@ const Lessons = () => {
             )}
           </div>
         ))}
-      </div>
-      <div className="w-full   mt-10 justify-center flex flex-row space-x-10 ">
-        <div className="bg-[#CCD5AE] h-56 w-56 p-3 rounded-lg">
-          <img src={stressbg} className="mb-4 rounded-lg"></img>
-          <Link
-            to={`/stress`}
-            className="text-black font-bold  hover:bg-white hover:rounded-md hover:p-2 hover:text-orange-600"
-          >
-            Stress
-          </Link>
-        </div>
-        <div className="bg-[#CCD5AE] h-56 w-56 p-3 rounded-lg">
-          <img src={depressionbg} className="mb-4 rounded-lg"></img>
-          <Link
-            to={`/stress`}
-            className="text-black font-bold  hover:bg-white hover:rounded-md hover:p-2 hover:text-orange-600"
-          >
-            Depressed
-          </Link>
-        </div>
-        <div className="bg-[#CCD5AE] h-56 w-56 p-3 rounded-lg">
-          <img src={anxietybg} className="mb-4 rounded-lg"></img>
-          <Link
-            to={`/stress`}
-            className="text-black font-bold  hover:bg-white hover:rounded-md hover:p-2 hover:text-orange-600"
-          >
-            Anxiety
-          </Link>
-        </div>
-      </div>
+</div>
     </section>
   );
 };
