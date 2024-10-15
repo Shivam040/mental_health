0e0b9b6852d55c647cfde099fc2d2f8b3b8fe3b0
Shivam Dangwal <shivamdangwal04@gmail.com>
2024-10-15 10:45:33 +0530

    done

--- client/src/pages/Lessons.jsx
+++ client/src/pages/Lessons.jsx
@@ -27,11 +27,19 @@ const Lessons = () => {
       question: "How does Tailwind work?",
       answer:
         "Tailwind works by providing a large set of utility classes that you can apply directly to HTML elements to style them.",
+<<<<<<< HEAD
+=======
+      color: "#00a896",
+>>>>>>> parent of 40dd3ca (Revert "css")
     },
     {
       question: "Can I customize Tailwind?",
       answer:
         "Yes, Tailwind is fully customizable. You can customize the design system by modifying the default configuration.",
+<<<<<<< HEAD
+=======
+      color: "#028090",
+>>>>>>> parent of 40dd3ca (Revert "css")
     },
   ];
   const toggleOpen = (index) => {
@@ -39,11 +47,16 @@ const Lessons = () => {
   };
 
   return (
+<<<<<<< HEAD
     <section
       id="lessons"
       className="w-full min-h-screen p-4 md:p-8"
     >
       <h1 className="h1-style mb-8 ">Check Up</h1>
+=======
+    <section id="lessons" className="w-full min-h-screen p-4 md:p-8">
+      
+>>>>>>> parent of 40dd3ca (Revert "css")
       {/* Banner */}
       <div className="mb-8 banner-container-style text-white text-shadow bg-gradient-to-r from-blue-600 to-blue-800">
         <div className="relative p-8 z-10">
@@ -64,6 +77,7 @@ const Lessons = () => {
             lesson={lesson}
           />
         ))}
+<<<<<<< HEAD
       </div>
       <div className="w-full   mt-10 justify-center">
         {faqData.map((faq, index) => (
@@ -86,6 +100,63 @@ const Lessons = () => {
           </div>
         ))}
 </div>
+=======
+      </div> */}
+
+<div className="w-full mt-10 flex flex-col justify-center ">
+  {faqData.map((faq, index) => (
+    <div
+      key={index}
+      className={`w-[630px] md:w-full py-2 px-4 h-20 shadow-md cursor-pointer transition-transform duration-300 ease-in-out transform ${
+        openIndex === index ? "h-24" : "hover:-translate-y-1 "
+      }`}
+      style={{ backgroundColor: faq.color }}  // Apply dynamic background color here
+      onClick={() => toggleOpen(index)}
+    >
+      {/* Question */}
+      <h3 className="text-lg text-white font-bold text-gray-800">{faq.question}</h3>
+
+      {/* Answer */}
+      {openIndex === index && (
+        <div className="mt-2 text-gray-600 ">
+          <p className="text-white">{faq.answer}</p>
+        </div>
+      )}
+    </div>
+  ))}
+</div>
+
+
+      <div className="w-full   mt-10 justify-center flex flex-row space-x-10 ">
+        <div className="bg-[#00A896] h-56 md:h-64 w-56 p-3 rounded-lg">
+          <img src={stressbg} className="mb-4 rounded-lg"></img>
+          <Link
+            to={`/stress`}
+            className="text-black font-bold  hover:bg-white hover:rounded-md hover:p-2 hover:text-orange-600"
+          >
+            Stress
+          </Link>
+        </div>
+        <div className="bg-[#02c39a] h-56 w-56 md:h-64 p-3 rounded-lg">
+          <img src={depressionbg} className="mb-4 rounded-lg"></img>
+          <Link
+            to={`/stress`}
+            className="text-black font-bold  hover:bg-white hover:rounded-md hover:p-2 hover:text-orange-600"
+          >
+            Depressed
+          </Link>
+        </div>
+        <div className="bg-[#F0F3BD] h-56 md:h-64 w-56 p-3 rounded-lg">
+          <img src={anxietybg} className="mb-4 rounded-lg"></img>
+          <Link
+            to={`/stress`}
+            className="text-black font-bold  hover:bg-white hover:rounded-md hover:p-2 hover:text-orange-600"
+          >
+            Anxiety
+          </Link>
+        </div>
+      </div>
+>>>>>>> parent of 40dd3ca (Revert "css")
     </section>
   );
 };
